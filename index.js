5053c7026c8046a11894b48409a9241d39a5400f
Pranshu Khanna <khanna.pranshu@yahoo.com>
2021-06-03 14:14:38 +0530

    my virtual card

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+Pranshu Khanna
+Website - https://www.pransh.tech
+Twitter - https://www.twitter.com/inmypranshoes
+LinkedIn - https://www.linkedin.com/in/pransh15
+Instagram - https://www.instagram.com/inmypranshoes
+Facebook - https://www.facebook.com/pransh15
+GitHub - https://www.github.com/pransh15
+Dev.to - https://www.dev.to/pransh15
+Email - pranshu@graphcms.com
+`)
